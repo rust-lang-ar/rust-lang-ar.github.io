562d595cbc84f2a2ab13a01ddf331392d899b71e
Esteban Borai <estebanborai@gmail.com>
2020-09-30 01:49:25 -0300

    add: yew config

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+use wasm_bindgen::prelude::*;
+use yew::prelude::*;
+
+struct Main {}
+
+impl Component for Main {
+    type Message = ();
+    type Properties = ();
+
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _: Self::Message) -> ShouldRender {
+        true
+    }
+
+    fn change(&mut self, _: Self::Properties) -> ShouldRender {
+        true
+    }
+
+    fn view(&self) -> Html {
+        html! { <span>{"Hello World!"}</span> }
+    }
+}
+
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    App::<Main>::new().mount_to_body();
+}
