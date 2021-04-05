97cabe86a83faca205ab07c475b3ee1c4cc24a2d
Esteban Borai <estebanborai@gmail.com>
2021-04-04 20:38:38 -0400

    chore: project scaffold
    Setup files scaffold to use Yew and Webpack.

--- bin/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-wasm-pack build --dev --target web --out-name wasm --out-dir ./static
---
+++ bootstrap.js
@@ -0,0 +1,5 @@
+import './src/style.scss';
+
+import('./pkg').then((mod) => {
+  mod.run_app();
+});
